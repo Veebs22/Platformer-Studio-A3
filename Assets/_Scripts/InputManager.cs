10f6ef2b4e40344de1da199dd44da43cfaf4ce42
Veebs22 <97552796+Veebs22@users.noreply.github.com>
2025-03-12 15:54:01 -0700

    quick control update

--- Assets/_Scripts/InputManager.cs
+++ Assets/_Scripts/InputManager.cs
@@ -12,6 +12,7 @@ public class InputManager : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.P)) {
             OnSettingsMenu?.Invoke();
         }
+        if (GameManager.Instance.IsSettingsMenuActive) { return; }
         Vector2 input = Vector2.zero;
         if (Input.GetKey(KeyCode.W))
         {
